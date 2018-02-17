0ce199ecbd61779c45b7b7d3d881b804bac203a6
Tania Kolesnik <kolesnik@gmail.com>
2018-02-17 22:05:42 +0000

    after review updates

--- app/src/main/java/com/example/android/quizanbd/MainActivity.java
+++ app/src/main/java/com/example/android/quizanbd/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.android.quizanbd;
 
-
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
